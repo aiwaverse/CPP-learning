4efe71e2549a730f82c83c5b9ca318313d2b8e00
Aiwa <aiwavision@protonmail.com>
2019-03-08 17:45:17 -0300

    Added Checking_Account

---
+++ Section15/Checking_Account.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "Savings_Account.hpp"
+
+class Checking_Account : public Account{
+public:
+    bool withdraw(double amount);
+    Checking_Account(std::string name, double balance);
+    friend std::ostream &operator<<(std::ostream &os, const Checking_Account &account);
+
+};
--- Section15/main.cpp
+++ Section15/main.cpp
@@ -1,8 +1,11 @@
 #include "Account.hpp"
 #include "Savings_Account.hpp"
+#include "Checking_Account.hpp"
 
 int main(void)
 {
+    Checking_Account carly {"Carly", 1000};
+    std::cout << carly << std::endl;
 
     std::cout << "This program ended successfully";
     return 0;
