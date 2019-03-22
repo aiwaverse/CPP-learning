f3fcb8970d3fc80babb02b878be94905ab4843e0
aiwaverse <aiwavision@protonmail.com>
2019-03-22 12:40:25 -0300

    Changed insertion to binary search, pure binary search function just for testing

--- Insertion_sort/insertion_sort.cpp
+++ Insertion_sort/insertion_sort.cpp
@@ -9,14 +9,15 @@
 int main(void)
 {
     srand(time(nullptr));
-    std::vector<int> vec {};
-    const size_t max_vector_size {30};
-    for(auto i{0};i<max_vector_size;++i)
-       vec.push_back(rand()%100);      //fills the vector with 30 random numbers
+    std::vector<int> vec { 8, 2,  7, 4 , 3, 0};
+ //   const size_t max_vector_size {30};
+ //   for(auto i{0};i<max_vector_size;++i)
+ //      vec.push_back(rand()%100);      //fills the vector with 30 random numbers
     auto count_of_operations = insertion_sort(vec);
-    for(auto v:vec)
-        std::cout << v << ", ";
-    std::cout << std::endl;
+    std::cout << "[ ";
+    for(auto const& v:vec)
+        std::cout << v << ' ';
+    std::cout << ']' << std::endl;
     std::cout << "operations: " << count_of_operations << std::endl;
     std::cout << "The program has ended totally fine, giiiirl" << std::endl;
     return 0;
