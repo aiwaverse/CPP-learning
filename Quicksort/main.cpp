73dbca2c1a8aa78c401f617d83646e2f520523f9
aiwaverse <aiwavision@protonmail.com>
2019-04-08 22:06:22 -0300

    changed the quicksort files, and measured time

--- Quicksort/main.cpp
+++ Quicksort/main.cpp
@@ -2,22 +2,46 @@
 
 int main(void)
 {
-    srand(time(nullptr));
-    std::vector<std::string> vec{"aiwa", "mimi", "carly", "luna", "mei"};
-    /*const int vector_size {10};
-    for(auto i{0};i < vector_size;++i)
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+    std::vector<int> vec{};
+    const int vector_size{100000};
+    std::uniform_int_distribution<long long> distribution(0, 1000);
+    for (auto i{0}; i < vector_size; ++i)
     {
-        vec.push_back((rand()%100)+1);
-    }*/
-    for(const auto& v: vec)
+        vec.push_back(distribution(generator));
+    }
+    /*for (const auto &v : vec)
         std::cout << v << " ";
-    std::cout << std::endl;
-    long long base_zero {0};
-    long long size = static_cast<long long int>(vec.size()-1);
-    quicksort(vec, base_zero, size);
-    for(const auto& v: vec)
+    std::cout << std::endl;*/
+    long long base_zero{0};
+    long long size = static_cast<long long int>(vec.size() - 1);
+    auto time = calculate_time(vec, base_zero, size, random_quicksort);
+    /*for (const auto &v : vec)
         std::cout << v << " ";
-    std::cout << std::endl;
+    std::cout << std::endl;*/
+    std::cout << "quicksort took " << time << " microseconds" << std::endl;
+    vec.clear();
+    for (auto i{0}; i < vector_size; ++i)
+    {
+        vec.push_back(distribution(generator));
+    }
+    time = calculate_time(vec, shellsort_ciura_basic);
+    std::cout << "shellsort took " << time << " microseconds" << std::endl;
+    vec.clear();
+    for (auto i{0}; i < vector_size; ++i)
+    {
+        vec.push_back(distribution(generator));
+    }
+    time = calculate_std_sort_time(vec);
+    std::cout << "std::sort took " << time << " microseconds" << std::endl;
+    vec.clear();
+    for (auto i{0}; i < vector_size; ++i)
+    {
+        vec.push_back(distribution(generator));
+    }
+    time = calculate_std_stable_sort_time(vec);
+    std::cout << "std::stable_sort took " << time << " microseconds" << std::endl;
     std::cout << "it works? it works" << std::endl;
     return 0;
 }
--- Quicksort/quicksort.hpp
+++ Quicksort/quicksort.hpp
@@ -1,8 +1,11 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <chrono>
+#include <random>
 #include <ctime>
 #include <string>
+#include <algorithm>
 
 template <typename T1>
 long long partition(std::vector<T1> &vector_to_be_partitioned, long long low, long long high)
@@ -25,6 +28,16 @@ long long partition(std::vector<T1> &vector_to_be_partitioned, long long low, lo
         std::swap(vector_to_be_partitioned.at(i), vector_to_be_partitioned.at(j));
     }
 }
+template <typename T>
+long long random_partition(std::vector<T>& vec, long long low, long long high)
+{
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+    std::uniform_int_distribution<long long> distribution(low+1, high-1);
+    auto r {distribution(generator)};
+    std::swap(vec.at(low), vec.at(r));
+    return partition(vec, low, high);
+}
 
 template <typename T1> //generic programming bc why not?
 void quicksort(std::vector<T1> &vector_to_be_ordered, long long low, long long high)
@@ -35,4 +48,72 @@ void quicksort(std::vector<T1> &vector_to_be_ordered, long long low, long long h
         quicksort(vector_to_be_ordered, low, p);
         quicksort(vector_to_be_ordered, p + 1, high);
     }
+}
+template <typename T>
+void random_quicksort(std::vector<T>& vec, long long low, long long high)
+{
+    if(high>low)
+    {
+        auto p = random_partition(vec, low, high);
+        quicksort(vec, low, p);
+        quicksort(vec, p+1, high);
+    }
+}
+template <typename T>
+size_t calculate_time(std::vector<T> &vec, long long low, long long high, void (*sort)(std::vector<T> &, long long low, long long high)) //using a function pointer HOORAY
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    sort(vec, low, high); //cute
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); //but it just calculates run time
+    return duration.count();
+}
+template <typename T>
+size_t calculate_time(std::vector<T> &vec, void (*sort)(std::vector<T> &)) //using a function pointer HOORAY
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    sort(vec); //cute
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); //but it just calculates run time
+    return duration.count();
+}
+void shellsort_ciura_basic(std::vector<int> &vec)
+{
+    std::vector<int> gap_list{1750, 701, 301, 132, 57, 23, 10, 4, 1}; //ciura's optimal list
+    while (static_cast<int>(vec.size()) >= gap_list.at(0))
+    {
+        int element = floor(gap_list.at(0) * 2.25);
+        gap_list.insert(gap_list.begin(), element);
+    }
+    for (auto gap : gap_list)
+    {
+        for (long unsigned i{static_cast<long unsigned>(gap)}; i < vec.size(); ++i) //cicyles the vector, starting at gap
+        {
+            int j{};
+            int temp = vec.at(i);
+            for (j = i; j >= gap and vec[j - gap] > temp; j -= gap)
+            {
+                vec.at(j) = vec.at(j - gap);
+            }
+            vec.at(j) = temp;
+        }
+    }
+}
+template <typename T>
+size_t calculate_std_sort_time(std::vector<T> &vec)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    std::sort(vec.begin(),vec.end());
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); //but it just calculates run time
+    return duration.count();
+}
+template <typename T>
+size_t calculate_std_stable_sort_time(std::vector<T> &vec)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    std::stable_sort(vec.begin(),vec.end());
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); //but it just calculates run time
+    return duration.count();
 }
