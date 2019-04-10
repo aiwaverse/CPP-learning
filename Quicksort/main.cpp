924fef0419d69666550c5383da7787490964cd17
aiwaverse <aiwavision@protonmail.com>
2019-04-09 21:46:14 -0300

    changed insertion sort, changed to google formatting

--- Quicksort/main.cpp
+++ Quicksort/main.cpp
@@ -1,47 +1,62 @@
 #include "quicksort.hpp"
 
-int main(void)
-{
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+int main(void) {
+    // sets up the seed, the generator, and the distribution for the random vector
+    // fill
+    auto seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
+    std::uniform_int_distribution<long long> distribution(0, 100);
+    /* Please use the following distribution if working with doubles, if with
+    another type, well... std::uniform_real_distribution<> distribution(0.0,
+    100.0);*/
+
     std::vector<int> vec{};
-    const int vector_size{100000};
-    std::uniform_int_distribution<long long> distribution(0, 1000);
-    for (auto i{0}; i < vector_size; ++i)
-    {
+    const int vector_size{10};
+    for (auto i{0}; i < vector_size; ++i) {
         vec.push_back(distribution(generator));
     }
-    /*for (const auto &v : vec)
-        std::cout << v << " ";
+    // just to print the vector before and after the quicksort, uncomment if the
+    // vector is smaller
+    /*for (const auto& v : vec)
+      std::cout << v << " ";
     std::cout << std::endl;*/
-    long long base_zero{0};
-    long long size = static_cast<long long int>(vec.size() - 1);
-    auto time = calculate_time(vec, base_zero, size, random_quicksort);
-    /*for (const auto &v : vec)
-        std::cout << v << " ";
+    size_t total_time{};
+    for (auto i{0}; i < 1000; ++i) {
+        auto time = calculate_time(vec, quicksort);
+        total_time += time;
+        vec.clear();
+        for (auto i{0}; i < vector_size; ++i) {
+            vec.push_back(distribution(generator));
+        }
+    }
+    /*for (const auto& v : vec)
+      std::cout << v << " ";
     std::cout << std::endl;*/
-    std::cout << "quicksort took " << time << " microseconds" << std::endl;
+    std::cout << "quicksort took " << total_time / 1000 << " microseconds as a mean" << std::endl;
     vec.clear();
-    for (auto i{0}; i < vector_size; ++i)
-    {
+    for (auto i{0}; i < vector_size; ++i) {
         vec.push_back(distribution(generator));
     }
-    time = calculate_time(vec, shellsort_ciura_basic);
-    std::cout << "shellsort took " << time << " microseconds" << std::endl;
-    vec.clear();
-    for (auto i{0}; i < vector_size; ++i)
-    {
-        vec.push_back(distribution(generator));
+    total_time = 0;
+    for (auto i{0}; i < 1000; ++i) {
+        auto time = calculate_time(vec, shellsort_ciura_basic);
+        total_time += time;
+        vec.clear();
+        for (auto i{0}; i < vector_size; ++i) {
+            vec.push_back(distribution(generator));
+        }
     }
-    time = calculate_std_sort_time(vec);
-    std::cout << "std::sort took " << time << " microseconds" << std::endl;
-    vec.clear();
-    for (auto i{0}; i < vector_size; ++i)
-    {
-        vec.push_back(distribution(generator));
-    }
-    time = calculate_std_stable_sort_time(vec);
-    std::cout << "std::stable_sort took " << time << " microseconds" << std::endl;
-    std::cout << "it works? it works" << std::endl;
+    std::cout << "shellsort took " << total_time / 1000 << " microseconds as a mean" << std::endl;
+    // simple code to test std::sort and compare efficiency
+    // can't use the same function since it works with iterators
+    /*{
+      auto start = std::chrono::high_resolution_clock::now();
+      std::sort(vec.begin(), vec.end());
+      auto stop = std::chrono::high_resolution_clock::now();
+      auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); std::cout << "std::sort took " << duration.count() << "
+      microseconds\n";
+    }*/
+
+    std::cout << "get it, hit it, kill it\n";
     return 0;
 }
--- Quicksort/quicksort.hpp
+++ Quicksort/quicksort.hpp
@@ -1,26 +1,22 @@
 #pragma once
-#include <iostream>
-#include <vector>
-#include <chrono>
-#include <random>
-#include <ctime>
-#include <string>
 #include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
 
-template <typename T1>
-long long partition(std::vector<T1> &vector_to_be_partitioned, long long low, long long high)
-{
-    T1 pivot{vector_to_be_partitioned.at((low + high) / 2)}; //asign to pivot the member of vector[(low+high)/2]
+template <typename T>
+long long partition(std::vector<T> &vector_to_be_partitioned, long long high, long long low) {
+    // this functions calculates partitions for quickosrt, it defines the pivot to the floor division of the mean of the boundaries
+    T pivot{vector_to_be_partitioned.at((low + high) / 2)};  // asign to pivot the member of vector[(low+high)/2]
     long long i{low - 1};
     long long j{high + 1};
-    while (true)
-    {
-        do
-        {
+    while (true) {
+        do {
             ++i;
         } while (vector_to_be_partitioned.at(i) < pivot);
-        do
-        {
+        do {
             --j;
         } while (vector_to_be_partitioned.at(j) > pivot);
         if (i >= j)
@@ -29,91 +25,70 @@ long long partition(std::vector<T1> &vector_to_be_partitioned, long long low, lo
     }
 }
 template <typename T>
-long long random_partition(std::vector<T>& vec, long long low, long long high)
-{
+long long random_partition(std::vector<T> &vec, long long high, long long low) {
+    /*this function is to be used with the random version, it swaps the first element
+with a random placed element, and then calls partition */
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
-    std::uniform_int_distribution<long long> distribution(low+1, high-1);
-    auto r {distribution(generator)};
+    std::uniform_int_distribution<long long> distribution(low + 1, high - 1);
+    auto r{distribution(generator)};
     std::swap(vec.at(low), vec.at(r));
-    return partition(vec, low, high);
+    return partition(vec, high, low);
 }
 
-template <typename T1> //generic programming bc why not?
-void quicksort(std::vector<T1> &vector_to_be_ordered, long long low, long long high)
-{
-    if (low < high)
-    {
-        long long p = partition(vector_to_be_ordered, low, high);
-        quicksort(vector_to_be_ordered, low, p);
-        quicksort(vector_to_be_ordered, p + 1, high);
+template <typename T>
+void quicksort(std::vector<T> &vector_to_be_ordered, long long high, long long low = 0) {
+    // normal quicksort function
+    if (low < high) {
+        long long p = partition(vector_to_be_ordered, high, low);
+        quicksort(vector_to_be_ordered, p, low);
+        quicksort(vector_to_be_ordered, high, p + 1);
     }
 }
 template <typename T>
-void random_quicksort(std::vector<T>& vec, long long low, long long high)
-{
-    if(high>low)
-    {
-        auto p = random_partition(vec, low, high);
-        quicksort(vec, low, p);
-        quicksort(vec, p+1, high);
+void random_quicksort(std::vector<T> &vec, long long low = 0) {
+    // quicksort but does with the random partitions, high is defined on the body to use with the calculate_time function
+    long long high = vec.size() - 1;
+    if (high > low) {
+        auto p = random_partition(vec, high, low);
+        quicksort(vec, p, low);
+        quicksort(vec, high, p + 1);
     }
 }
 template <typename T>
-size_t calculate_time(std::vector<T> &vec, long long low, long long high, void (*sort)(std::vector<T> &, long long low, long long high)) //using a function pointer HOORAY
-{
+size_t calculate_time(std::vector<T> &vec, void (*sort)(std::vector<T> &)) {
+    // using a function pointer to calculate execution time of a function, returning in microseconds
+    // can be changed to nanoseconds if wished
     auto start = std::chrono::high_resolution_clock::now();
-    sort(vec, low, high); //cute
+    sort(vec);
     auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); //but it just calculates run time
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     return duration.count();
 }
 template <typename T>
-size_t calculate_time(std::vector<T> &vec, void (*sort)(std::vector<T> &)) //using a function pointer HOORAY
-{
-    auto start = std::chrono::high_resolution_clock::now();
-    sort(vec); //cute
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); //but it just calculates run time
-    return duration.count();
-}
-void shellsort_ciura_basic(std::vector<int> &vec)
-{
-    std::vector<int> gap_list{1750, 701, 301, 132, 57, 23, 10, 4, 1}; //ciura's optimal list
-    while (static_cast<int>(vec.size()) >= gap_list.at(0))
-    {
+void shellsort_ciura_basic(std::vector<T> &vec) {
+    // same normal shellsort program, but with templates to fit the idea
+    std::vector<int> gap_list{1750, 701, 301, 132, 57, 23, 10, 4, 1};  // ciura's optimal list
+    while (static_cast<int>(vec.size()) >= gap_list.at(0)) {
         int element = floor(gap_list.at(0) * 2.25);
         gap_list.insert(gap_list.begin(), element);
     }
-    for (auto gap : gap_list)
-    {
-        for (long unsigned i{static_cast<long unsigned>(gap)}; i < vec.size(); ++i) //cicyles the vector, starting at gap
+    for (auto gap : gap_list) {
+        for (long unsigned i{static_cast<long unsigned>(gap)}; i < vec.size(); ++i)  // cicyles the vector, starting at gap
         {
             int j{};
-            int temp = vec.at(i);
-            for (j = i; j >= gap and vec[j - gap] > temp; j -= gap)
-            {
+            T temp = vec.at(i);
+            for (j = i; j >= gap and vec[j - gap] > temp; j -= gap) {
                 vec.at(j) = vec.at(j - gap);
             }
             vec.at(j) = temp;
         }
     }
 }
+// overloaded both the quicksort functions
+// this is here so we can call it on the calculate_time function
+// since that function only accepts a void function with a reference to a vector as parameter
 template <typename T>
-size_t calculate_std_sort_time(std::vector<T> &vec)
-{
-    auto start = std::chrono::high_resolution_clock::now();
-    std::sort(vec.begin(),vec.end());
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); //but it just calculates run time
-    return duration.count();
-}
+void quicksort(std::vector<T> &vec) { quicksort(vec, vec.size() - 1); }
 template <typename T>
-size_t calculate_std_stable_sort_time(std::vector<T> &vec)
-{
-    auto start = std::chrono::high_resolution_clock::now();
-    std::stable_sort(vec.begin(),vec.end());
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); //but it just calculates run time
-    return duration.count();
-}
+void random_quicksort(std::vector<T> &vec) { quicksort(vec, vec.size() - 1); }
