65135a9c7d8ebe83b4675a0f5cee964c68a98796
aiwaverse <aiwavision@protonmail.com>
2019-04-12 21:48:20 -0300

    hopefully fixed the annoying commit

--- NewQuicksort/main.cpp
+++ NewQuicksort/main.cpp
@@ -1,7 +1,8 @@
 #include "quicksort.hpp"
 
+using namespace mei;
+
 int main(){
-<<<<<<< HEAD
     mei::sort_data<int> vec;
     vec.push_back(3);
     vec.push_back(5);
@@ -10,11 +11,6 @@ int main(){
     std::cout << vec;
     vec.quicksort(0,vec.size()-1);
     std::cout << vec;
-=======
-    mei::sort_data<int> data;
-    data.push_back(5);
-    std::cout << data;
->>>>>>> 4bdb666e8b4bde04126d2f1652c42649bc9ee00f
     std::cout << "podo podohae" << std::endl;
     return 0;
 }
--- NewQuicksort/quicksort.hpp
+++ NewQuicksort/quicksort.hpp
@@ -1,23 +1,15 @@
-<<<<<<< HEAD
 #include <algorithm>
 #include <array>
 #include <chrono>
 #include <fstream>
 #include <iostream>
 #include <random>
-=======
-#include <fstream>
-#include <iostream>
-#include <random>
-#include <chrono>
->>>>>>> 4bdb666e8b4bde04126d2f1652c42649bc9ee00f
 #include <string>
 #include <vector>
 
 namespace mei {
 template <typename Sort_T>
 class sort_data {
-<<<<<<< HEAD
     //THIS TOOK AWHILE Y'KNOW
     template <typename T>
     friend std::ostream& operator<<(std::ostream& os, const sort_data<T>& rhs);
@@ -81,31 +73,4 @@ void sort_data<T>::quicksort(long long low, long long high) {
         quicksort(p + 1, high);
     }
 }
-=======
-    friend std::ostream& operator<<(std::ostream &os, sort_data<Sort_T>& rhs);
- private:
-  std::vector<Sort_T> data{};
-  long long recursions{};
-  long long swaps{};
-  long long partition_median();
-  long long random_partition();
-
- public:
-  sort_data() : data{}, recursions{}, swaps{}{};
-  void fill_random();
-  void push_back(Sort_T n) {
-    data.push_back(n);
-  }
-  void quicksort(long long low, long long high);
-};
-std::ostream& operator<<(std::ostream &os, sort_data<Sort_T>& rhs)
-{
-    os << "[ ";
-    for(auto v:rhs.data)
-        os << v << " ";
-    os << "]\n";
-    os << "Recursion calls : " << rhs.recursions << "Swaps: " << rhs.swaps << std::endl;
-    return os;
-}
->>>>>>> 4bdb666e8b4bde04126d2f1652c42649bc9ee00f
 }  // namespace mei
