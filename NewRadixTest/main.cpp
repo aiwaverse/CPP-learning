03873046a6c195e95fc0e417331f107362734107
aiwaverse <aiwavision@protonmail.com>
2019-04-25 17:06:26 -0300

    fixed the new radix files, but don't use them

--- NewRadixTest/main.cpp
+++ NewRadixTest/main.cpp
@@ -1,10 +1,10 @@
 #include "radix.hpp"
 
 int main(void) {
-    std::vector<std::string> letters{"ab", "abc", "abcd", "a", "batata"};
-    display(letters);
-    sort_string(letters, 0, letters.size()-1, 0);
-    display(letters);
+    std::vector<int> numbers{ 12312, 812, 192, 0, 21, 123, 4812};
+    display(numbers);
+    radix(numbers);
+    display(numbers);
     std::cout << "love myself today" << std::endl;
     return 0;
 }
--- NewRadixTest/radix.hpp
+++ NewRadixTest/radix.hpp
@@ -17,6 +17,3 @@ void _radix(std::vector<int>& to_sort, unsigned expo);
 void fill_random(std::vector<int> &vec);
 int64_t test_radix_new_time(std::vector<int>& vec);
 int64_t test_radix_old_time(std::vector<int>& vec);
-void _string_radix_msd(std::vector<std::string>& words, size_t index);
-void string_radix_msd(std::vector<std::string>& words);
-void sort_string(std::vector<std::string>& a, int lo, int hi, int d);
